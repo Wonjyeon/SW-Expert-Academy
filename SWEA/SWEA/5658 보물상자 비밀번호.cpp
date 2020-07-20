14e6bd6b433550a062507d6f5af1c77aa433e80d
Won-Juyeon <34434489+Wonjyeon@users.noreply.github.com>
2020-07-21 00:00:03 +0900

    Update 5658 보물상자 비밀번호.cpp

--- SWEA/SWEA/5658 보물상자 비밀번호.cpp
+++ SWEA/SWEA/5658 보물상자 비밀번호.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
-#include <cstring>
 #include <vector>
-#include <set>
-#include <algorithm>
 #include <string>
-#include <functional>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-	set<string,greater<string>> s;
-	int T, N, K;
-	char arr[29];
-	cin >> T;
-	for (int tc = 1; tc <= T; tc++)
-	{
-		cin >> N >> K;
-		string str = "";
-		string ans = "";
-		cin >> str;
-		int cnt = 0;
-		int num = N / 4;
-		str = str.append(str);
+int T, N, K, ans;
+string str;
 
-		for (int i = 0; i < N; i++)
-		{
-			string str2 = str.substr(i, num);
-			s.insert(str2);
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		vector<string> v;
+		cin >> N >> K >> str;
+		int len = N / 4;
+		ans = 0;
+		str += str.substr(0, len - 1);
+
+		for (int i = 0; i <= str.length() - len; i++) {
+			v.push_back(str.substr(i, len));
 		}
-		for (auto it : s)
-		{
-			cnt++;
-			if (cnt == K)
-				ans = it;
+		sort(v.rbegin(), v.rend());
+		v.erase(unique(v.begin(), v.end()), v.end());
+		string ans_str = v[K - 1];
+
+		for (int i = 0; i < len; i++) {
+			char c = ans_str[i];
+			if (c - '0' >= 0 && c - '0' <= 9)
+				ans += pow(16, (len - i - 1)) * (c - '0');
+			else
+				ans += pow(16, (len - i - 1)) * (c - '7');
 		}
-		s.clear();
-		char cstr[8];
-		strcpy(cstr, ans.c_str());
-		int ten_ans = (int)strtol(cstr, NULL, 16);
-		cout << "#" << tc << " " << ten_ans << endl;
+		cout << '#' << tc << ' ' << ans << '\n';
 	}
+	return 0;
 }
